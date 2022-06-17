c8861b801d8ebbdb3df7487cde4056231e52ea18
Ben Plate <bplate9583@gmail.com>
2022-06-17 00:19:15 -0700

    improve simulation performance substantially

--- rust-src/lib.rs
+++ rust-src/lib.rs
@@ -3,7 +3,6 @@ extern crate lazy_static;
 
 mod utils;
 
-use std::ptr;
 use std::sync::RwLock;
 use wasm_bindgen::prelude::*;
 use js_sys::Array;
@@ -19,7 +18,7 @@ const TIME_STEP: f64 = 0.2;
 const NUM_SIMS_PER_STEP: i32 = 5;
 
 lazy_static! {
-    static ref UNIVERSE: RwLock<Vec<Body<'static>>> = RwLock::new(vec![]);
+    static ref UNIVERSE: RwLock<Vec<Body>> = RwLock::new(vec![]);
 }
 
 #[derive(Clone, Copy)]
@@ -28,79 +27,62 @@ pub struct Vector2D {
     y: f64,
 }
 
-pub struct Body<'a> {
+#[derive(Clone, Copy)]
+pub struct Body {
     mass: f64,
     position: Vector2D,
     velocity: Vector2D,
-    scene: &'a RwLock<Vec<Body<'a>>>,
 }
 
-impl Body<'_> {
-    fn next_velocity(&self) -> Vector2D {
+impl Body {
+    fn next_velocity(&mut self, uni: &Vec<Body>) {
         if self.mass.abs() < 0.000001 {
-            return self.velocity;
+            return;
         }
 
-        let mut out: Vector2D = self.velocity;
+        // let mut out: Vector2D = self.velocity;
 
-        let uni = self.scene.read().unwrap();
+
         for i in 0..uni.len() {
-            if !ptr::eq(&uni[i], self) && uni[i].mass.abs() > 0.000001 {
+            if uni[i].mass.abs() > 0.000001 {
                 let dx = self.position.x - uni[i].position.x;
                 let dy = self.position.y - uni[i].position.y;
 
+                let dist = dx.powf(2.0) + dy.powf(2.0);
+                if dist < 0.0001 { continue; }
+
                 let angle = dy.atan2(dx);
 
-                let delta_v = BIG_G * uni[i].mass / (dx.powf(2.0) + dy.powf(2.0)) * TIME_STEP; // (G(m1)(m2)/d^2) / m1 * t = G(m2)/d^2 * t = at = delta_v
+                let delta_v = (BIG_G * uni[i].mass / dist) * TIME_STEP; // (G(m1)(m2)/d^2) / m1 * t = G(m2)/d^2 * t = at = delta_v
 
-                out.x += delta_v * -angle.cos();
-                out.y += delta_v * -angle.sin();
+                self.velocity.x += delta_v * -angle.cos();
+                self.velocity.y += delta_v * -angle.sin();
             }
         }
 
-        return out;
+        // self.velocity = out;
     }
 
-    fn next_position(&self) -> Vector2D {
-        return Vector2D { x: self.position.x + self.velocity.x * TIME_STEP, y: self.position.y + self.velocity.y * TIME_STEP }; // x_new = x_old + vt
+    fn next_position(&mut self) {
+        self.position.x += self.velocity.x * TIME_STEP;
+        self.position.y += self.velocity.y * TIME_STEP;
+        // return Vector2D { x: self.position.x + self.velocity.x * TIME_STEP, y: self.position.y + self.velocity.y * TIME_STEP }; // x_new = x_old + vt
     }
 }
 
 #[wasm_bindgen]
 pub fn step_time() {
-    let mut next_velocities: Vec<Vector2D> = vec![];
-    let mut next_positions: Vec<Vector2D> = vec![];
-    {
-        let uni = UNIVERSE.read().unwrap();
-        for i in 0..uni.len() {
-            next_velocities.push(uni[i].velocity);
-            next_positions.push(uni[i].position);
-        }
-    }
+    let mut uni = UNIVERSE.write().unwrap();
+    let num_bodies = uni.len();
+
     for _ in 0..NUM_SIMS_PER_STEP {
-        {
-            let uni = UNIVERSE.read().unwrap();
-            for i in 0..uni.len() {
-                next_velocities[i] = uni[i].next_velocity();
-            }
+        for i in 0..num_bodies {
+            let mut body = uni[i];
+            body.next_velocity(&uni);
+            uni[i] = body;
         }
-        {
-            let mut uni = UNIVERSE.write().unwrap();
-            for i in 0..uni.len() {
-                uni[i].velocity = next_velocities[i];
-            }
-        }
-        {
-            let uni = UNIVERSE.read().unwrap();
-            for i in 0..uni.len() {
-                next_positions[i] = uni[i].next_position();
-            }
-        }
-        {
-            let mut uni = UNIVERSE.write().unwrap();
-            for i in 0..uni.len() {
-                uni[i].position = next_positions[i];
-            }
+        for i in 0..num_bodies {
+            uni[i].next_position();
         }
     }
 }
@@ -117,7 +99,6 @@ pub fn add_body(mass: f64, position_x: f64, position_y: f64, velocity_x: f64, ve
             x: velocity_x,
             y: velocity_y,
         },
-        scene: &UNIVERSE,
     };
 
     UNIVERSE.write().unwrap().push(new_body);
